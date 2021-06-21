a7e3698bc4a5a3d8ad40021acd254dad67367b57
mohamaddabti <72363251+mohamaddabti@users.noreply.github.com>
2021-06-22 01:03:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Route, BrowserRouter, Switch} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -6,12 +6,37 @@ import Post from "./components/Post";
 import Project from "./components/Project";
 import SinglePost from "./components/SinglePost";
 import NavBar from "./components/NavBar";
+import Dropdown from './components/Dropdown';
+
 
 
 function App() {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resized');
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+  
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
         <Switch>
           <Route component={Home} path = '/' exact />
           <Route component={About} path = '/about'/>
