85bc8346df1737b76a64dd35a343c36c115ec5a2
mohamaddabti <72363251+mohamaddabti@users.noreply.github.com>
2021-06-22 01:01:51 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,10 @@ export default function Home(){
         <main>
             <video
                 src={video} alt="Nordlys" autoPlay loop muted
-                className="absolute object-cover w-full h-full" 
+                className="absolute object-cover w-full h-full sm:absolute " 
             />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h4 className="text-2xl text-indigo-100 font-bold cursivie leading-none lg:leading-snug"> Hello. I am Mohammad. </h4>
+                <h4 className="text-2xl text-indigo-100 font-bold cursivie leading-none lg:leading-snug"> Hello, I am Mohammad. </h4>
             </section>
         </main>
     )
