bb5ab5bf67ab7532875e26cc213c533a82996537
mohamaddabti <mohamad.dabty@gmail.com>
2021-12-28 18:20:31 +0100

    fix

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-//import { Link } from "react-router-dom";
 import sanityClient  from "../client.js";
 import '../index.css'
 import '../Slider.css'
