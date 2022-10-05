5a94df4388cea7a38af2f0f1fe9c242c2989dcec
mohamaddabti <mohamad.dabty@gmail.com>
2022-10-05 00:00:11 -0400

    navbar fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,99 @@ import '../index.css'
 const Navbar = ({ toggle }) => {
   return (
     
-    <nav id='navColor'
+    
+        <nav id='navColor' className="">
+          <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <div className="flex items-center">
+              <Link to='/' className='self-center text-4xl  text-gray-900 hover:text-gray-50 font-bond cursive'>
+          
+              {"{"}MOHAMMAD DABTI{"}"}
+              </Link>
+            </div>
+
+            <div className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' onClick={toggle}>
+              <svg
+                className='w-8 h-8'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16M4 18h16'
+                />
+              </svg>
+            </div>
+
+            <div className='hidden justify-between items-center w-full md:flex md:w-auto '>
+              <div className='flex flex-col p-2 mt-40 rounded-lg md:flex-row md:space-x-2 md:mt-0 md:text-sm md:font-medium '>
+
+              
+                <Link to='/project' className='block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0 text-gray-900 p-2 hover:text-gray-50' >
+                  Projects
+                </Link>
+                <Link to='/post' className='block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0 text-gray-900 p-2 hover:text-gray-50 '>
+                  Blog
+                </Link>
+                <Link to='/gallery' className='block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0 text-gray-900 p-2 hover:text-gray-50' >
+                  Gallery
+                </Link>
+                <Link to='/Contact' className='block py-2 pr-4 pl-3 rounded md:bg-transparent md:p-0 text-gray-900 p-2 hover:text-gray-50' >
+                  Contact
+                </Link>
+                
+                <SocialIcon 
+                  url="https://www.facebook.com/mohammad.dabti/" 
+                  className="mr-4" 
+                  target="_blank" 
+                  fgColor="#fff" 
+                  style={{height: 35,width: 35}} 
+                />
+                <SocialIcon 
+                  url="https://www.youtube.com/user/MohamadDabti" 
+                  className="mr-4" 
+                  target="_blank" 
+                  fgColor="#fff" 
+                  style={{height: 35,width: 35}} 
+                />
+                <SocialIcon 
+                  url="https://twitter.com/MDabti" 
+                  className="mr-4" 
+                  target="_blank" 
+                  fgColor="#fff" 
+                  style={{height: 35,width: 35}} 
+                />
+                <SocialIcon 
+                  url="https://www.linkedin.com/in/mohammad-dabti-509476215/" 
+                  className="mr-4" 
+                  target="_blank" 
+                  fgColor="#fff" 
+                  style={{height: 35,width: 35}} 
+                />
+                <SocialIcon 
+                  url="https://github.com/mohamaddabti" 
+                  className="mr-4" 
+                  target="_blank" 
+                  fgColor="#fff" 
+                  style={{height: 35,width: 35}} 
+                />
+            </div>
+          </div>
+          </div>
+        </nav>
+
+
+
+
+
+    /*<nav id='navColor'
       className='navbar flex-wrap h-16 shadow p-2 rounded'
       role='navigation'
-    >
-      <Link to='/' className=' text-4xl text-gray-900 hover:text-gray-50 font-bond cursive'>
+      >
+      <Link to='/' className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
         
       {"{"}MOHAMMAD DABTI{"}"}
       </Link>
@@ -33,16 +121,16 @@ const Navbar = ({ toggle }) => {
       </div>
       <div className='blog pr-1 lg:block  hidden activeClassName="text-gray-100 bg-gray-700" text-white'>
         
-        <Link to='/project' className='project text-gray-900 p-4 hover:text-gray-50' >
+        <Link to='/project' className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white' >
           Projects
         </Link>
-        <Link to='/post' className='text-gray-900 p-4 hover:text-gray-50'>
+        <Link to='/post' className='tblock py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'>
           Blog
         </Link>
-        <Link to='/gallery' className='Gallery text-gray-900 p-4 hover:text-gray-50' >
+        <Link to='/gallery' className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white' >
           Gallery
         </Link>
-        <Link to='/Contact' className='about text-gray-900 p-4  hover:text-gray-50' >
+        <Link to='/Contact' className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white' >
           Contact
         </Link>
         
@@ -82,7 +170,7 @@ const Navbar = ({ toggle }) => {
           style={{height: 35,width: 35}} 
         />
       </div>
-    </nav>
+    </nav>*/
   );
 };
 
